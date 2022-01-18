9a1129a6100e6f49f429920d7f6f0f9944d8b948
Jonathan Cantor <jonatcantor@gmail.com>
2022-01-18 15:54:46 -0400

    Download favicon in png 64x64

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,19 +6,36 @@ import { FaSave } from 'react-icons/fa';
 import { useState } from 'react';
 
 const Main = () => {
-  const [drawCanvas, setDrawCanvas] = useState(false);
+  const [drawCanvas, setDrawCanvas] = useState('BLANK');
+  const [canvasData, setCanvasData] = useState('');
 
   const handleClickRandom = () => {
     setDrawCanvas(true);
   }
 
-  const handleClickSave = () => {
-    console.log('click!');
+  const handleClickSave = async () => {
+    const response = await fetch(process.env.REACT_APP_API, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      method: 'POST',
+      body: JSON.stringify({ 'image': canvasData }),
+    });
+
+    const jsonData = await response.json();
+    const imageData = URL.createObjectURL(await (await fetch(jsonData.image)).blob());
+
+    const downloadImage = document.createElement('a');
+    downloadImage.href = imageData;
+    downloadImage.download = 'favicon.png';
+    downloadImage.click();
+    downloadImage.remove();
   }
 
   return (
     <Wrapper>
-      <Canvas isDraw={ drawCanvas } setDrawCanvas={ setDrawCanvas } width='300' height='300' />
+      <Canvas isDraw={ drawCanvas } setDrawCanvas={ setDrawCanvas } setCanvasData={ setCanvasData } width='300' height='300' />
       <Button handleClick={ handleClickRandom } icon={ <GiPerspectiveDiceSixFacesRandom size='3rem' fill='#F05454' /> } />
       <Button handleClick={ handleClickSave } icon={ <FaSave size='2.5rem' fill='#325288' /> } />
     </Wrapper>
